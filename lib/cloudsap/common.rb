30f228a4192a5535294153069753d4dd921235b7
Brian Warsing <dayglojesus@gmail.com>
2020-11-02 21:42:37 -0800

    build out diff AWS clients and CLI options

--- lib/cloudsap/common.rb
+++ lib/cloudsap/common.rb
@@ -2,6 +2,32 @@
 
 module Cloudsap
   module Common
+    class << self
+      attr_reader :options
+
+      def aws_iam_client
+        @iam_client = Aws::IAM::Client.new(
+          region: options.aws_region,
+        )
+      end
+
+      def aws_eks_client
+        @eks_client = Aws::EKS::Client.new(
+          region: aws_region,
+        )
+      end
+
+      def aws_sts_client
+        @sts_client = Aws::STS::Client.new(
+          region: aws_region,
+        )
+      end
+
+      def options=(hash)
+        @options = hash.transform_keys(&:to_sym)
+      end
+    end
+
     def kubeconfig
       config = ENV['KUBECONFIG'] || File.expand_path('~/.kube/config')
       Kubeclient::Config.read(config)
